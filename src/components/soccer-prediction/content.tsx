f4f6af0e24b5ad75bd2beff084d5af0a1a93e791
Mariel García <117679688+marieeeel@users.noreply.github.com>
2025-05-18 17:25:04 -0400

    chore: create components folder structure

---
+++ src/components/soccer-prediction/content.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import DesktopContent from './desktop/desktop-content';
+//import MobileContent from './mobile/mobile-content';
+
+const Content: React.FC = () => {
+  return (
+    <>
+      <div className="hidden lg:block"></div>
+
+      <div className="lg:hidden"></div>
+    </>
+  );
+};
+
+export default Content;
