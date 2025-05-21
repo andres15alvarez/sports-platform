a8e445a62da6da9c4b8e457017e5da36429e2835
Andres Alvarez <andres15alvarez@gmail.com>
2025-05-21 10:02:09 -0400

    Configure images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,24 @@ const nextConfig: NextConfig = {
   webpack(config, options) {
     return config;
   },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'logos-world.net',
+      '1000logos.net',
+      'placehold.co',
+      'upload.wikimedia.org',
+      'i.pravatar.cc',
+      'upload.wikimedia.org',
+      'seeklogo.com',
+      'images.seeklogo.com',
+      'www.citypng.com',
+      'sportslogos.net',
+    ],
+
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextTranslate(nextConfig);
