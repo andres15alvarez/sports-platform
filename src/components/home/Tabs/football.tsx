30e690f80203aa38231ecfc8901dfe5f1477d347
yaiscg <yaisraylith@gmail.com>
2025-06-05 09:31:59 -0400

    feat: football odds page

--- src/components/home/Tabs/football.tsx
+++ src/components/home/Tabs/football.tsx
@@ -179,7 +179,7 @@ const FootballTab: React.FC = () => {
 
       <div className="text-center mt-4">
         <a
-          href="/football-predictions"
+          href="/football-prediction"
           className="inline-block bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md font-medium"
         >
           View all football predictions
