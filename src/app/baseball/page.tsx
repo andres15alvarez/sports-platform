05b9a898aeedd3bae999665cf71b7e9a86ec397a
Mariel García <117679688+marieeeel@users.noreply.github.com>
2025-06-06 22:45:31 -0400

    add routes

---
+++ src/app/baseball/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BaseballPage() {
+  return (
+    <main>
+      <h1>Baseball</h1>
+    </main>
+  );
+}
---
+++ src/app/basketball/[slug-league]/standings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StandingsPage() {
+  return (
+    <main>
+      <h1>Standings</h1>
+    </main>
+  );
+}
